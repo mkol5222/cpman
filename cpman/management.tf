a8e00eeb499d953ee89979024c4d725ad7f85ad2
mkol5222 <mkoldov@gmail.com>
2025-03-28 06:42:45 +0000

    cpman config hints

--- cpman/management.tf
+++ cpman/management.tf
@@ -19,11 +19,18 @@ module "example_module" {
     management_GUI_client_network   = "0.0.0.0/0"
     mgmt_enable_api                 = "all"
     admin_password                  = var.cpadmin_password
+    # https://learn.microsoft.com/en-us/azure/virtual-machines/sizes/overview
+    # az vm list-sizes --location westeurope --output table | grep Standard_D3_v2
     vm_size                         = "Standard_D3_v2" # 4 vCPU, 14GB RAM
     disk_size                       = "110"
+    # az vm image list-publishers --location westeurope --output table | grep checkpoint
+    # az vm image list-offers --location westeurope --publisher checkpoint --output table | grep cg-r82
+    # az vm image list-skus --location westeurope --publisher checkpoint --offer check-point-cg-r82 --output table
+    # az vm image list --location westeurope --publisher checkpoint --offer check-point-cg-r82 --sku mgmt-byol --all --output table
+
     vm_os_sku                       = "mgmt-byol"
-    vm_os_offer                     = "check-point-cg-r8120"
-    os_version                      = "R8120"
+    vm_os_offer                     = "check-point-cg-r82" # "check-point-cg-r8120"
+    os_version                      = "R82" # R8120"
     bootstrap_script                = "touch /home/admin/bootstrap.txt; echo 'hello_world' > /home/admin/bootstrap.txt"
     allow_upload_download           = true
     authentication_type             = "Password"
